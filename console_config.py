43113410fb96be1460784d1d6ba9ee2022e6a8f3
Lucas crl <crendallucas1@gmail.com>
2024-12-14 15:03:37 +0100

    Update console_config.py

--- console_config.py
+++ console_config.py
@@ -3,18 +3,22 @@ import colorlog
 
 def setup_console(name):
     logger = logging.getLogger(name)
-    handler = colorlog.StreamHandler()
-    handler.setFormatter(colorlog.ColoredFormatter(
-        '%(log_color)s[%(levelname)s]%(reset)s %(asctime)s - %(message)s',
-        datefmt='%d-%m-%Y %H:%M:%S',
+    logger.setLevel(logging.DEBUG)  # Capture all levels of logs
+
+    # Create colorlog StreamHandler
+    console_handler = colorlog.StreamHandler()
+    console_handler.setFormatter(colorlog.ColoredFormatter(
+        '%(log_color)s[%(asctime)s] (%(name)s) %(message)s',
+        datefmt='%m/%d/%y %H:%M:%S',
         log_colors={
             'DEBUG': 'cyan',
             'INFO': 'green',
             'WARNING': 'yellow',
             'ERROR': 'red',
             'CRITICAL': 'red,bg_white',
-            'COMMANDS': 'blue',
-        }))
-    logger.addHandler(handler)
-    logger.setLevel(logging.INFO)
+        },
+        reset=True,
+        style='%'
+    ))
+    logger.addHandler(console_handler)
     return logger
