2082146a1bb0f87498d0ca39f078504385df4814
Lucas crl <crendallucas1@gmail.com>
2024-12-16 16:13:26 +0100

    Optimisation des Extension bot.py
    Signed-off-by: Lucas crl <crendallucas1@gmail.com>

--- bot.py
+++ bot.py
@@ -28,85 +28,33 @@ intents.guilds = True
 
 bot = commands.Bot(command_prefix='!', intents=intents, help_command=CustomHelp())
 
-async def load_extensions():
+
+async def load_extensions(bot):
     try:
         db.setup_database()
         db.setup_radio_database()
         db.setup_invites_database()
-
         logger.info("La base de données est configurée.")
-        await bot.load_extension("commands.music")
-        await bot.load_extension("commands.server")
-        await bot.load_extension("commands.setup")
-        await bot.load_extension("commands.invite")    
-        logger.info("Extensions commands chargées avec succès.")
-        await bot.load_extension("events.errors")
-        await bot.load_extension("events.ready")
-        await bot.load_extension("events.logger")
-        await bot.load_extension("events.join")
-        await bot.load_extension("events.leave")
-        await bot.load_extension("events.invitetracker")
-        await bot.load_extension("events.discordlogger")
-        logger.info("Extensions events chargées avec succès.")
+
+        # Liste des fichiers à exclure du chargement
+        exclude_files = ['__init__.py', 'help.py']
+
+        extensions_path = ['commands', 'events']
+        for path in extensions_path:
+            for file in os.listdir(path):
+                if file.endswith('.py') and file not in exclude_files:
+                    extension = f"{path}.{file[:-3]}"
+                    try:
+                        await bot.load_extension(extension)
+                        logger.info(f"Extension {extension} chargée avec succès.")
+                    except Exception as e:
+                        logger.error(f"Erreur lors du chargement de l'extension {extension}: {e}")
+
         console_manager = ConsoleCommandManager(bot)
-        console_manager.start() 
+        console_manager.start()
     except Exception as e:
-        logger.error(f"Impossible de charger les extensions : {e}")
-                
-import discord
-from discord.ext import commands
-import os
-from dotenv import load_dotenv
-from database import Database  
-from commands.help import CustomHelp
-from discord.ext import tasks
-from console_config import setup_console 
-import signal
-import asyncio
-from consolecommandmanager import ConsoleCommandManager
+        logger.error(f"Erreur globale lors du chargement des extensions : {e}")
 
-logger = setup_console(__name__)
-load_dotenv()
-TOKEN = os.getenv("DISCORD_TOKEN")
-db = Database(
-    host=os.getenv("HOST"),
-    user=os.getenv("UTILISATEUR"),
-    password=os.getenv("PASSWORD"),
-    database=os.getenv("DATABASE"),
-    port=os.getenv("PORT")
-)
-
-intents = discord.Intents.default()
-intents.message_content = True
-intents.members = True
-intents.guilds = True
-
-bot = commands.Bot(command_prefix='!', intents=intents, help_command=CustomHelp())
-
-async def load_extensions():
-    try:
-        db.setup_database()
-        db.setup_radio_database()
-        db.setup_invites_database()
-
-        logger.info("La base de données est configurée.")
-        await bot.load_extension("commands.music")
-        await bot.load_extension("commands.server")
-        await bot.load_extension("commands.setup")
-        await bot.load_extension("commands.invite")    
-        logger.info("Extensions commands chargées avec succès.")
-        await bot.load_extension("events.errors")
-        await bot.load_extension("events.ready")
-        await bot.load_extension("events.logger")
-        await bot.load_extension("events.join")
-        await bot.load_extension("events.leave")
-        await bot.load_extension("events.invitetracker")
-        await bot.load_extension("events.discordlogger")
-        logger.info("Extensions events chargées avec succès.")
-        console_manager = ConsoleCommandManager(bot)
-        console_manager.start() 
-    except Exception as e:
-        logger.error(f"Impossible de charger les extensions : {e}")
                 
 async def shutdown(signal, loop):
     print(f'Reçu du signal {signal.name}...')
@@ -117,11 +65,11 @@ async def shutdown(signal, loop):
 
 async def main():
     loop = asyncio.get_running_loop()
-    if os.name != 'nt':  # Not Windows
+    if os.name != 'nt':
         for sig in [signal.SIGTERM, signal.SIGINT]:
             loop.add_signal_handler(sig, lambda s=sig: asyncio.create_task(shutdown(s, loop)))
     try:
-        await load_extensions()
+        await load_extensions(bot)
         logger.info("Démarrage du bot Discord...")
         await bot.start(TOKEN)
     except KeyboardInterrupt:
